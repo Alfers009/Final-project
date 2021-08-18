79901683af660dc9c10c053319c188786ad424e3
Alfers009 <74427063+Alfers009@users.noreply.github.com>
2021-08-18 21:02:18 +0300

    18.08

--- Final project/js/quicksand/setting.js
+++ Final project/js/quicksand/setting.js
@@ -1,30 +1,27 @@
 jQuery.noConflict();
-jQuery(document).ready(function($){
+jQuery(document).ready(function ($) {
 
-if (jQuery().quicksand) {
+	if (jQuery().quicksand) {
 
- 	
-	var $data = $(".portfolio-area").clone();
-	
-	
-	$('.portfolio-categ li').click(function(e) {
-		$(".filter li").removeClass("active");	
-		
-		var filterClass=$(this).attr('class').split(' ').slice(-1)[0];
-		
-		if (filterClass == 'all') {
-			var $filteredData = $data.find('.item-thumbs');
-		} else {
-			var $filteredData = $data.find('.item-thumbs[data-type=' + filterClass + ']');
-		}
-		$(".portfolio-area").quicksand($filteredData, {
-			duration: 600,
-			adjustHeight: 'auto'
-		}
+		var $data = $(".portfolio-area").clone();
+
+		$('.portfolio-categ li').click(function (e) {
+			$(".filter li").removeClass("active");
+			var filterClass = $(this).attr('class').split(' ').slice(-1)[0];
+
+			if (filterClass == 'all') {
+				var $filteredData = $data.find('.item-thumbs');
+			} else {
+				var $filteredData = $data.find('.item-thumbs[data-type=' + filterClass + ']');
+			}
+			$(".portfolio-area").quicksand($filteredData, {
+				duration: 600,
+				adjustHeight: 'auto'
+			}	
 		$(this).addClass("active");
-		return false;
-	});
-	
-}
+			return false;
+		});
+
+	}
 
 });
